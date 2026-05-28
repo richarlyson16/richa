eb588ccc38be5ac251a5c5e112abed8067ebc315
richarlyson16 <richarlysonwilliam@gmail.com>
2026-05-28 18:51:09 -0300

    Update health.ts

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,10 +1,9 @@
 import { Router, type IRouter } from "express";
-import { HealthCheckResponse } from "@workspace/api-zod";
 
 const router: IRouter = Router();
 
 router.get("/healthz", (_req, res) => {
-  const data = HealthCheckResponse.parse({ status: "ok" });
+  const data = any.parse({ status: "ok" });
   res.json(data);
 });
 
